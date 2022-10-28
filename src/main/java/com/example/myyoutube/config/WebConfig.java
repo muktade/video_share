9f7f5864f521503fbfe9a08a23027d7abf22a8e0
muktadeer <muktadeer@gmail.com>
2022-10-29 00:53:23 +0600

    login_style.css ok

---
+++ src/main/java/com/example/myyoutube/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.idb.tasks.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("http://localhost:4200")
+                .allowCredentials(true)
+                .allowedHeaders("Origin", "Access-Control-Allow-Origin", "Content-Type",
+                        "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+                        "Access-Control-Request-Method", "Access-Control-Request-Headers")
+                .exposedHeaders("Origin", "Content-Type", "Accept", "Authorization",
+                        "Access-Control-Allow-Origin", "Access-Control-Allow-Origin",
+                        "Access-Control-Allow-Credentials")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH");
+    }
+}
